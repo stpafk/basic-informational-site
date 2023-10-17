4c9882bf43c2c511cda978b67a3e9684213ff1f5
stpafk <stephan.weigert@hotmail.com>
2023-10-17 08:45:20 -0300

    fix https

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-const http = require('http');
-const fs = require('fs').promises
+const http = require("http");
+const fs = require("fs");
 
-http.createServer(function(req, res) {
+http
+  .createServer(function (req, res) {
+
+    let filename = "";
     if (req.url === "/") {
-        res.writeHead(200, {'Content-Type': "text/html"});
-        res.write("<p>Hello</>")
-        return res.end();
-    } 
-
-    else {
-        res.writeHead(500, {'Content-Type': 'text/html'});
-        res.write("<p>Error</>");
-        return res.end()
+      filename = "." + "/index.html";
+    } else {
+      filename = "." + req.url;
     }
-}).listen(8080)
+
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write("<h1>404 Not Found</h1>");
+        return res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      }
+    });
+  })
+  .listen(8080);
